ec753435fc1be9186b22798012203c9124794a8c
oti-favour <favourgraceoti@gmail.com>
2023-09-19 10:26:20 +0100

    blog section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -148,7 +148,7 @@ carousel.addEventListener("mouseleave", () => {
 });
 
 function startAutoSlide() {
-	interval = setInterval(next, 5000); // Adjust the interval as needed (3 seconds in this example)
+	interval = setInterval(next, 2000); // Adjust the interval as needed (3 seconds in this example)
 }
 
 let interval;
